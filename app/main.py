e610e0b5fc54bfb333db5b10914ec0dabd6bb2f9
Franco Prieto <fpa936@gmail.com>
2026-07-03 19:35:18 -0400

    refactor: migrate general endpoints to layered architecture

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 from app.database import init_db
 from app.models import Contact, CallSession, Message
 from app.api.v1.endpoints_webhooks import router as webhooks_router
-from app.api.v1.endpoints_general import router as general_router
+from app.modules.general.routes import router as general_router
 from app.api.v1.endpoints_n8n import router as n8n_router
 from app.modules.contacts.routes import router as contacts_module_router
 
